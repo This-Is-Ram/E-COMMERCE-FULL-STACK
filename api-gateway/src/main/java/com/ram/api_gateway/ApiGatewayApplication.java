5aeae683750d4e1a7058ca6fc0bdf3e4116dd43f
Ram <durgaramannapureddy@gmail.com>
2026-06-05 17:53:48 +0530

    Uncomment and enable ApiGatewayApplication class

--- api-gateway/src/main/java/com/ram/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ram/api_gateway/ApiGatewayApplication.java
@@ -1,56 +1,12 @@
-// package com.ram.api_gateway;
+package com.ram.api_gateway;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
-// public class ApiGatewayApplication {
+@SpringBootApplication
+public class ApiGatewayApplication {
 
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(ApiGatewayApplication.class, args);
-// 	}
-// }
-
-
-package com.ram.api_gateway.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "https://ram-e-commerce-microservices.netlify.app"
-        ));
-
-        config.setAllowedMethods(List.of(
-                "GET",
-                "POST",
-                "PUT",
-                "DELETE",
-                "OPTIONS"
-        ));
-
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsWebFilter(source);
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
