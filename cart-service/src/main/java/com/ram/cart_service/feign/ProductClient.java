db4b1e43b85afcfbc70f1d7ff78baedb6afaab2e
Ram <durgaramannapureddy@gmail.com>
2026-06-05 16:03:30 +0530

    Update ProductClient to use external URL

--- cart-service/src/main/java/com/ram/cart_service/feign/ProductClient.java
+++ cart-service/src/main/java/com/ram/cart_service/feign/ProductClient.java
@@ -5,8 +5,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "PRODUCT-SERVICE",configuration = FeignConfig.class)
+@FeignClient(
+        name = "product-service",
+        url = "https://product-service-e3qx.onrender.com"
+)
 public interface ProductClient {
+
     @GetMapping("/api/products/{id}")
     ProductResponse getProductById(@PathVariable Long id);
 }
