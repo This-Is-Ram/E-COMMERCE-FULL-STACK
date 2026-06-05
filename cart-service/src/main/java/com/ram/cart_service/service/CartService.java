dc23304bdc87091abed20288ec70d3be67b1585c
Ram <durgaramannapureddy@gmail.com>
2026-06-05 17:05:02 +0530

    Add import for List in CartService

--- cart-service/src/main/java/com/ram/cart_service/service/CartService.java
+++ cart-service/src/main/java/com/ram/cart_service/service/CartService.java
@@ -1,5 +1,7 @@
 package com.ram.cart_service.service;
 
+
+import java.util.List;
 import com.ram.cart_service.feign.ProductClient;
 import com.ram.cart_service.model.AddToCartRequest;
 import com.ram.cart_service.model.CartResponse;
